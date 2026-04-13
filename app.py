d4007b5eeab8c5756ecbcf018b7bed2ed2f7a502
Bhakti Mate <bhaktimate27@gmail.com>
2026-04-13 15:33:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(page_title="Smart Water Dashboard", layout="wide")
+
+st.title("💧 Smart Water Quality System")
+
+st.markdown("""
+### 🚀 Welcome
+
+This system helps you:
+- Analyze water quality using AI
+- View trends across Maharashtra cities
+- Get smart suggestions
+
+👉 Use sidebar to navigate:
+- Analysis Page
+- Dashboard Page
+""")
